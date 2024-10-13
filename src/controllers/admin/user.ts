99b691b0c1a23b658448f461557c86cd9c8cd4c6
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2024-10-13 07:00:28 +0700

    feat: add admin user router

--- src/controllers/admin/user.ts
+++ src/controllers/admin/user.ts
@@ -1,14 +1,32 @@
 import { Router } from 'express';
 import { deactivateUser } from '~/services/admin/user';
 import { tokenExtractor } from '~/middlewares/auth';
+import { deleteUser, getAllUser } from '~/services/user/user';
+import { getAllProfile } from '~/services/profile';
 
 const adminUserRouter = Router();
-adminUserRouter.use(tokenExtractor('ADMIN'));
+// adminUserRouter.use(tokenExtractor('ADMIN'));
 
 adminUserRouter.patch('/deactivate', async (req, res) => {
   const { id } = req.body;
   await deactivateUser({ id });
   res.status(200).json();
+});
+
+adminUserRouter.get('/get-all-user', async (req, res) => {
+  const users = await getAllUser();
+  res.status(200).json(users);
+});
+
+adminUserRouter.delete('/delete-user', async (req, res) => {
+  const { id } = req.body;
+  await deleteUser({ id });
+  res.status(200).json();
+});
+
+adminUserRouter.get('/get-all-profile', async (req, res) => {
+  const profiles = await getAllProfile();
+  res.status(200).json(profiles);
 });
 
 export default adminUserRouter;
