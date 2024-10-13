7b371b6487f71fae68263a6c22a9e369418218df
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2024-10-13 15:35:53 +0700

    feat: add category, measurement, food router

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import { errorConverter, errorHandler } from './middlewares/error-handler';
+import connectCloudinary from './utils/cloudinary';
 
 import userAuthRouter from './controllers/user/auth';
 import adminAuthRouter from './controllers/admin/auth';
 import adminUserRouter from './controllers/admin/user';
 import profileRouter from './controllers/profile';
-import connectCloudinary from './utils/cloudinary';
+import categoryRouter from './controllers/admin/category';
+import measurementRouter from './controllers/admin/measurement';
+import foodRouter from './controllers/food';
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,9 +36,14 @@ app.use('/user/auth', userAuthRouter);
 // admin routers
 app.use('/admin/auth', adminAuthRouter);
 app.use('/admin/user', adminUserRouter);
+app.use('/admin/category', categoryRouter);
+app.use('/admin/measurement', measurementRouter);
 
 // profile router
 app.use('/profile', profileRouter);
+
+// food router
+app.use('/food', foodRouter);
 
 // convert error to ApiError, if needed
 app.use(errorConverter);
